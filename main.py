2b58bd7152833e4101fba859c5f871bf8ec4fb43
icecube127 <kevin.an82@gmail.com>
2022-06-10 17:40:54 -0500

    requirements

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from venv import create
 from website import create_app
-from flask import Flask
 
-#hope this
+#hope this works!
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
